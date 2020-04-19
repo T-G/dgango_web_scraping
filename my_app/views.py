5bb8b70d667b0bc31f0466952c861ea2c77c4b83
Theo <pronoygomes@yahoo.com>
2020-04-19 16:56:55 +0100

    modified new_search.html file, refactor the base.html

--- my_app/views.py
+++ my_app/views.py
@@ -5,4 +5,8 @@ def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-    return render(request, 'my_app/new_search.html')
+    search = request.POST.get('search')
+    frontend_context = {
+        'search':search
+    }
+    return render(request, 'my_app/new_search.html', frontend_context)
